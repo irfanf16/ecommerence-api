21064146b6718d7c9186febe80b5fd70d12b792a
irfanf16 <70797386+irfanf16@users.noreply.github.com>
2023-09-20 01:20:40 +0500

    v

--- app/Http/Controllers/User/AppHomeScreenController.php
+++ app/Http/Controllers/User/AppHomeScreenController.php
@@ -53,38 +53,38 @@ class AppHomeScreenController extends Controller
 
         $categories = CategoryResource::collection($categories);
 
-        $brands = Brand::select('id', 'name', 'name_ar', 'logo_image', 'slug')
-            ->limit(12)
-            ->where(['status'=> 1,'featured'=>1])
-            ->inRandomOrder()
-            ->get();
-        $brands = BrandResource::collection($brands);
+//        $brands = Brand::select('id', 'name', 'name_ar', 'logo_image', 'slug')
+//            ->limit(12)
+//            ->where(['status'=> 1,'featured'=>1])
+//            ->inRandomOrder()
+//            ->get();
+//        $brands = BrandResource::collection($brands);
 
-        $random_banner= WebsiteBanner::where('status', 1)
-            ->inRandomOrder()
-            ->first();
+//        $random_banner= WebsiteBanner::where('status', 1)
+//            ->inRandomOrder()
+//            ->first();
 
-        $top_featured_subcategories=SubCategory::where(['status'=>1,'featured'=>1])->take(3)->inRandomOrder()->get();
+//        $top_featured_subcategories=SubCategory::where(['status'=>1,'featured'=>1])->take(3)->inRandomOrder()->get();
+//
+//        $top_featured_subcategories=SubCategoryResource::collection($top_featured_subcategories);
 
-        $top_featured_subcategories=SubCategoryResource::collection($top_featured_subcategories);
-
-        $featured_products = Product::with('firstVariant','image')
-            ->whereHas('firstVariant', function ($query) {
-                $query->where('quantity', '>=', 1)->where('availability', 1);
-            })
-            ->select('id', 'name', 'name_ar', 'slug', 'primary_image', 'brand_id', 'likes', 'views', 'sales', 'reports', 'total_reviews', 'avg_rating')
-            ->where([
-                'status' => 1,
-                'featured' => 1
-            ])
-            ->where('store_id', $store_id)
-
-//                ->whereRelation('store', 'is_verified', '=', 1)->whereRelation('store', 'status', '=', 1)
-            ->inRandomOrder()
-            ->take(8)
-            ->get();
-
-        $featured_products = ProductResource::collection($featured_products);
+//        $featured_products = Product::with('firstVariant','image')
+//            ->whereHas('firstVariant', function ($query) {
+//                $query->where('quantity', '>=', 1)->where('availability', 1);
+//            })
+//            ->select('id', 'name', 'name_ar', 'slug', 'primary_image', 'brand_id', 'likes', 'views', 'sales', 'reports', 'total_reviews', 'avg_rating')
+//            ->where([
+//                'status' => 1,
+//                'featured' => 1
+//            ])
+//            ->where('store_id', $store_id)
+//
+////                ->whereRelation('store', 'is_verified', '=', 1)->whereRelation('store', 'status', '=', 1)
+//            ->inRandomOrder()
+//            ->take(8)
+//            ->get();
+//
+//        $featured_products = ProductResource::collection($featured_products);
 
 
 //        $popular_products = Product::with('firstVariant','image')
@@ -116,47 +116,47 @@ class AppHomeScreenController extends Controller
 //            ->get();
 //        $new_added_products = ProductResource::collection($new_added_products);
 
-        $sale_cate=Category::where('slug','sales')->first();
-        $new_arrival_products = Product::with('firstVariant','image')
-            ->whereHas('firstVariant', function ($query) {
-                $query->where('quantity', '>=', 1)->where('availability', 1);
-            })
-            ->select('id', 'name', 'name_ar', 'slug', 'primary_image', 'brand_id', 'likes', 'views', 'sales', 'reports', 'total_reviews', 'avg_rating')
-            ->where([
-                'status' => 1,
-            ])
-            ->where('store_id', $store_id)
-            ->where('category_id', $sale_cate->id)
-//                ->whereRelation('store', 'is_verified', '=', 1)->whereRelation('store', 'status', '=', 1)
-            ->inRandomOrder()
-            ->take(8)
-            ->get();
-        $new_arrival_products = ProductResource::collection($new_arrival_products);
+//        $sale_cate=Category::where('slug','sales')->first();
+//        $new_arrival_products = Product::with('firstVariant','image')
+//            ->whereHas('firstVariant', function ($query) {
+//                $query->where('quantity', '>=', 1)->where('availability', 1);
+//            })
+//            ->select('id', 'name', 'name_ar', 'slug', 'primary_image', 'brand_id', 'likes', 'views', 'sales', 'reports', 'total_reviews', 'avg_rating')
+//            ->where([
+//                'status' => 1,
+//            ])
+//            ->where('store_id', $store_id)
+//            ->where('category_id', $sale_cate->id)
+////                ->whereRelation('store', 'is_verified', '=', 1)->whereRelation('store', 'status', '=', 1)
+//            ->inRandomOrder()
+//            ->take(8)
+//            ->get();
+//        $new_arrival_products = ProductResource::collection($new_arrival_products);
 
-        $rand_sub_category=SubCategory::has('products')->inRandomOrder()->take(1)->get();
+//        $rand_sub_category=SubCategory::has('products')->inRandomOrder()->take(1)->get();
+//
+//        $rand_sub_products = Product::with('firstVariant','image')
+//            ->whereHas('firstVariant', function ($query) {
+//                $query->where('quantity', '>=', 1)->where('availability', 1);
+//            })
+//            ->select('id', 'name', 'name_ar', 'slug', 'primary_image', 'brand_id', 'likes', 'views', 'sales', 'reports', 'total_reviews', 'avg_rating')
+//            ->where([
+//                'status' => 1,
+//            ])
+//            ->where('store_id', $store_id)
+//            ->where('subcategory_id', $rand_sub_category[0]->id)
+////                ->whereRelation('store', 'is_verified', '=', 1)->whereRelation('store', 'status', '=', 1)
+//            ->inRandomOrder()
+//            ->take(4)
+//            ->get();
 
-        $rand_sub_products = Product::with('firstVariant','image')
-            ->whereHas('firstVariant', function ($query) {
-                $query->where('quantity', '>=', 1)->where('availability', 1);
-            })
-            ->select('id', 'name', 'name_ar', 'slug', 'primary_image', 'brand_id', 'likes', 'views', 'sales', 'reports', 'total_reviews', 'avg_rating')
-            ->where([
-                'status' => 1,
-            ])
-            ->where('store_id', $store_id)
-            ->where('subcategory_id', $rand_sub_category[0]->id)
-//                ->whereRelation('store', 'is_verified', '=', 1)->whereRelation('store', 'status', '=', 1)
-            ->inRandomOrder()
-            ->take(4)
-            ->get();
-
-        $rand_sub_products = ProductResource::collection($rand_sub_products);
-        $rand_sub_category = SubCategoryResource::collection($rand_sub_category);
-
-        $random_sub_category_with_products=[
-            'category'=>$rand_sub_category,
-            'products'=>$rand_sub_products
-        ];
+//        $rand_sub_products = ProductResource::collection($rand_sub_products);
+//        $rand_sub_category = SubCategoryResource::collection($rand_sub_category);
+//
+//        $random_sub_category_with_products=[
+//            'category'=>$rand_sub_category,
+//            'products'=>$rand_sub_products
+//        ];
 
         $women_cate=Category::where('slug','womens')->first();
         $women_child_category=ChildCategory::with('category','subcategory')->where('category_id',$women_cate->id)->inRandomOrder()
@@ -168,21 +168,27 @@ class AppHomeScreenController extends Controller
             ->take(8)->get();
         $men_child_category = ChildCategoryResource::collection($men_child_category);
 
-        
+        $sales_cate=Category::where('slug','sales')->first();
+        $sales_child_category=ChildCategory::with('category','subcategory')->where('category_id',$sales_cate->id)->inRandomOrder()
+            ->take(8)->get();
+        $sales_child_category = ChildCategoryResource::collection($sales_child_category);
+
+
         return response()->json([
             'status' => 200,
             'covers' => $covers,
             'categories' => $categories,
-            'brands' => $brands,
-            'random_banner' => $random_banner,
-            'top_featured_subcategories' => $top_featured_subcategories,
-            'featured_products' => $featured_products,
+//            'brands' => $brands,
+//            'random_banner' => $random_banner,
+//            'top_featured_subcategories' => $top_featured_subcategories,
+//            'featured_products' => $featured_products,
 //            'popular_products' => $popular_products,
 //            'new_added_products' => $new_added_products,
-            'new_arrival_products' => $new_arrival_products,
-            'random_sub_category_with_products'=>$random_sub_category_with_products,
+//            'new_arrival_products' => $new_arrival_products,
+//            'random_sub_category_with_products'=>$random_sub_category_with_products,
             'women_child_category'=>$women_child_category,
-            'men_child_category'=>$men_child_category
+            'men_child_category'=>$men_child_category,
+            'sales_child_category'=>$sales_child_category,
         ]);
 
 //        } catch (\Throwable $th) {
